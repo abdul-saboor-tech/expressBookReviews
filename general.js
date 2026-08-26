9a90e73d35f0e01b8d744488e3609b5754aca59d
abdul-saboor-tech <abdulsaboorkhan293@gmail.com>
2026-08-26 18:54:34 +0500

    Fix Axios book retrieval functions

--- general.js
+++ general.js
@@ -1,21 +1,56 @@
 const axios = require("axios");
 
-// Promise-based helper for retrieving remote JSON data.
+// Promise-based Axios helper
 function getBooksWithPromise(url) {
-  return axios.get(url)
-    .then(response => response.data)
-    .catch(error => {
+  return axios
+    .get(url)
+    .then((response) => response.data)
+    .catch((error) => {
       throw error;
     });
 }
 
-// Async/await version.
+// Async/await Axios helper
 async function getBooksWithAsyncAwait(url) {
-  const response = await axios.get(url);
-  return response.data;
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+// Retrieve all books
+function getAllBooks(url) {
+  return getBooksWithPromise(`${url}/books`);
+}
+
+// Retrieve book by ISBN
+function getBooksByISBN(url, isbn) {
+  return getBooksWithAsyncAwait(
+    `${url}/books/isbn/${encodeURIComponent(isbn)}`
+  );
+}
+
+// Retrieve books by author
+function getBooksByAuthor(url, author) {
+  return getBooksWithPromise(
+    `${url}/books/author/${encodeURIComponent(author)}`
+  );
+}
+
+// Retrieve books by title
+function getBooksByTitle(url, title) {
+  return getBooksWithAsyncAwait(
+    `${url}/books/title/${encodeURIComponent(title)}`
+  );
 }
 
 module.exports = {
   getBooksWithPromise,
-  getBooksWithAsyncAwait
+  getBooksWithAsyncAwait,
+  getAllBooks,
+  getBooksByISBN,
+  getBooksByAuthor,
+  getBooksByTitle,
 };
